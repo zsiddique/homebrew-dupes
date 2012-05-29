3a7df965321512c7768249cbfbba1f3af133f727
Jack Nagel <jacknagel@gmail.com>
2012-05-28 23:10:43 -0500

    vim 7.3.531
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- vim.rb
+++ vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :revision => '1d1cce57421c'
-  version '7.3.502'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-531'
+  version '7.3.531'
 
   head 'https://vim.googlecode.com/hg/'
 
