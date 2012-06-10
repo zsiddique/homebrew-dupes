bcec2733089eba12a93ed1a74efb536f57b681b4
Joseph Wayne Norton <norton@alum.mit.edu>
2012-06-11 00:34:42 +0900

    New formula: expect
    Closes #40.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ expect.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Expect < Formula
+  homepage 'http://expect.sourceforge.net'
+  url 'http://downloads.sourceforge.net/project/expect/Expect/5.45/expect5.45.tar.gz'
+  md5 '44e1a4f4c877e9ddc5a542dfa7ecc92b'
+
+  def options
+    [['--enable-threads', 'Build with multithreading support']]
+  end
+
+  def install
+    args = ["--prefix=#{prefix}", "--exec-prefix=#{prefix}", "--mandir=#{man}"]
+    args << "--enable-shared"
+    args << "--enable-threads" if ARGV.include? "--enable-threads"
+    args << "--enable-64bit" if MacOS.prefer_64_bit?
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+
+  def test
+    system "#{bin}/mkpasswd"
+  end
+end
