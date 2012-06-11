287b315a3c43c44c7e74b02da879689351386f50
Jack Nagel <jacknagel@gmail.com>
2012-06-11 16:30:25 -0500

    libarchive: update homepage
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- libarchive.rb
+++ libarchive.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Libarchive < Formula
+  homepage 'http://libarchive.github.com/'
   url 'https://github.com/downloads/libarchive/libarchive/libarchive-3.0.4.tar.gz'
-  homepage 'http://code.google.com/p/libarchive/'
   md5 'af443ca9a10ddbcbf00f7ae34ca7fc16'
 
   def install
