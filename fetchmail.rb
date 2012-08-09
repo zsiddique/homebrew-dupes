5d3e296ce6d0af72fb1ab532fb1d62da9ea21312
samueljohn <github@samueljohn.de>
2012-08-09 12:24:44 +0200

    fetchmail: download location changed
    Apparently they now just redirect to sourceforge.
    
    Closes #65.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- fetchmail.rb
+++ fetchmail.rb
@@ -2,8 +2,10 @@ require 'formula'
 
 class Fetchmail < Formula
   homepage 'http://www.fetchmail.info/'
-  url 'http://prdownload.berlios.de/fetchmail/fetchmail-6.3.21.tar.bz2'
-  md5 '548f152df27d32b632afa4f1a8a6cd1a'
+  url 'http://downloads.sourceforge.net/project/fetchmail/branch_6.3/fetchmail-6.3.21.tar.xz'
+  sha1 '4f1e5f2e1a616d4007da1de9c976c2c1fb5dcb81'
+
+  depends_on 'xz' => :build
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
