8363396a5c0061f5ec293cf7d9b2c6264a7fc944
Max Howell <mxcl@me.com>
2012-08-11 22:50:20 -0400

    zlib builds on CLT-less rigs

--- zlib.rb
+++ zlib.rb
@@ -7,8 +7,26 @@ class Zlib < Formula
 
   keg_only :provided_by_osx
 
+  def patches; DATA end
+
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
 end
+
+# /usr/bin/libtool is no use to CLT-less rigs
+__END__
+diff --git a/configure b/configure
+index 36c7d8e..0c97d7b 100755
+--- a/configure
++++ b/configure
+@@ -231,7 +231,7 @@ if test "$gcc" -eq 1 && ($cc -c $cflags $test.c) >> configure.log 2>&1; then
+              SHAREDLIBV=libz.$VER$shared_ext
+              SHAREDLIBM=libz.$VER1$shared_ext
+              LDSHARED=${LDSHARED-"$cc -dynamiclib -install_name $libdir/$SHAREDLIBM -compatibility_version $VER1 -current_version $VER3"}
+-             AR="/usr/bin/libtool"
++             AR="libtool"
+              ARFLAGS="-o" ;;
+   *)             LDSHARED=${LDSHARED-"$cc -shared"} ;;
+   esac
