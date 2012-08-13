98329217739b6b2a3712c2b48aa0b74d0d85b74b
Jack Nagel <jacknagel@gmail.com>
2012-08-13 16:56:04 -0500

    expect: use options DSL
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- expect.rb
+++ expect.rb
@@ -5,14 +5,12 @@ class Expect < Formula
   url 'http://downloads.sourceforge.net/project/expect/Expect/5.45/expect5.45.tar.gz'
   md5 '44e1a4f4c877e9ddc5a542dfa7ecc92b'
 
-  def options
-    [['--enable-threads', 'Build with multithreading support']]
-  end
+  option 'enable-threads', 'Build with multithreading support'
 
   def install
     args = ["--prefix=#{prefix}", "--exec-prefix=#{prefix}", "--mandir=#{man}"]
     args << "--enable-shared"
-    args << "--enable-threads" if ARGV.include? "--enable-threads"
+    args << "--enable-threads" if build.include? "enable-threads"
     args << "--enable-64bit" if MacOS.prefer_64_bit?
 
     system "./configure", *args
