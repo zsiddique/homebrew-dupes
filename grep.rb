78980e6b466fb5bf1999c65be2d0751db6308187
Jack Nagel <jacknagel@gmail.com>
2012-08-20 22:33:52 -0500

    grep 2.14
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- grep.rb
+++ grep.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Grep < Formula
   homepage 'http://www.gnu.org/software/grep/'
-  url 'http://ftpmirror.gnu.org/grep/grep-2.13.tar.xz'
-  mirror 'http://ftp.gnu.org/gnu/grep/grep-2.13.tar.xz'
-  sha256 '461718cbd3f471cfd4c167aa7c31be72d9c18760b88186b5e0d1ab8345022dd0'
+  url 'http://ftpmirror.gnu.org/grep/grep-2.14.tar.xz'
+  mirror 'http://ftp.gnu.org/gnu/grep/grep-2.14.tar.xz'
+  sha256 'e70e801d4fbb16e761654a58ae48bf5020621c95c8e35bd864742577685872e1'
 
   depends_on 'xz' => :build
   depends_on 'pcre'
