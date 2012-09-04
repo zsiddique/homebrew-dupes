28743b9d68d2af37c71c03bbe85ebb5008c6d924
Jack Nagel <jacknagel@gmail.com>
2012-09-04 18:04:23 -0500

    lsof 4.86
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- lsof.rb
+++ lsof.rb
@@ -2,12 +2,12 @@ require 'formula'
 
 class Lsof < Formula
   homepage 'http://people.freebsd.org/~abe/'
-  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.85.tar.bz2'
-  md5 '102ee2081172bbe76dccaa6cceda8573'
+  url 'ftp://sunsite.ualberta.ca/pub/Mirror/lsof/lsof_4.86.tar.bz2'
+  sha1 '53c124fe6fddbc96f4724abec16572d9239f913e'
 
   def install
-    system "tar xf lsof_4.85_src.tar"
-    cd "lsof_4.85_src" do
+    system "tar xf lsof_4.86_src.tar"
+    cd "lsof_4.86_src" do
       mv "00README", "../README"
       system "./Configure -n darwin"
       system "make"
