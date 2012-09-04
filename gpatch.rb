40193ce9e0c00a08b10bd0dcfb8b69cf0a1111cd
Jack Nagel <jacknagel@gmail.com>
2012-09-04 17:51:57 -0500

    gpatch 2.6.1-188
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gpatch.rb
+++ gpatch.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Gpatch < Formula
   homepage 'http://savannah.gnu.org/projects/patch/'
   # Use a slightly newer version with build fixes
-  url 'http://alpha.gnu.org/gnu/patch/patch-2.6.1.136-31a7.tar.gz'
-  md5 '80975a976d9adbcacb61fe6b002c559d'
+  url 'http://alpha.gnu.org/gnu/patch/patch-2.6.1.188-66ae.tar.gz'
   version '2.6.1'
+  sha1 'caac60a80abc915b8d17d236d601e36238f7068a'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
