74d1d53cd066746142722ba42439e634fa2d593e
Jack Nagel <jacknagel@gmail.com>
2012-09-04 17:46:12 -0500

    fetchmail 6.3.22
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- fetchmail.rb
+++ fetchmail.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Fetchmail < Formula
   homepage 'http://www.fetchmail.info/'
-  url 'http://downloads.sourceforge.net/project/fetchmail/branch_6.3/fetchmail-6.3.21.tar.xz'
-  sha1 '4f1e5f2e1a616d4007da1de9c976c2c1fb5dcb81'
+  url 'http://downloads.sourceforge.net/project/fetchmail/branch_6.3/fetchmail-6.3.22.tar.xz'
+  sha1 '3bb755010dbe568d53840e22d8e87d8d5db25b19'
 
   depends_on 'xz' => :build
 
