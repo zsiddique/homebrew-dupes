8319830c0faa13d6e58e61f8468f97293f9b4f5e
Jack Nagel <jacknagel@gmail.com>
2012-09-04 17:46:25 -0500

    less 451
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- less.rb
+++ less.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Less < Formula
   homepage 'http://www.greenwoodsoftware.com/less/index.html'
-  url 'http://www.greenwoodsoftware.com/less/less-444.tar.gz'
-  md5 '56f9f76ffe13f70155f47f6b3c87d421'
+  url 'http://www.greenwoodsoftware.com/less/less-451.tar.gz'
+  sha1 'ee95be670e8fcc97ac87d02dd1980209130423d0'
 
   def install
     system "./configure", "--prefix=#{prefix}"
