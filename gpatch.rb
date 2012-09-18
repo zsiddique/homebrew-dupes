6f8701b5032ece9645f86e54e1b4505d22b706e7
Jack Nagel <jacknagel@gmail.com>
2012-09-18 11:32:53 -0500

    patch 2.7
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- gpatch.rb
+++ gpatch.rb
@@ -2,10 +2,9 @@ require 'formula'
 
 class Gpatch < Formula
   homepage 'http://savannah.gnu.org/projects/patch/'
-  # Use a slightly newer version with build fixes
-  url 'http://alpha.gnu.org/gnu/patch/patch-2.6.1.188-66ae.tar.gz'
-  version '2.6.1'
-  sha1 'caac60a80abc915b8d17d236d601e36238f7068a'
+  url 'http://ftpmirror.gnu.org/patch/patch-2.7.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/patch/patch-2.7.tar.bz2'
+  sha1 '3a08bb3a6f3a5f3a03cbb3bfa052528a1a259ec8'
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
