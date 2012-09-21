cecef0350a3ddac50678ad89a86338d4df190b87
Jack Nagel <jacknagel@gmail.com>
2012-09-20 22:46:07 -0500

    tidy: needs autotools
    Fixes #72.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- tidy.rb
+++ tidy.rb
@@ -5,6 +5,9 @@ class Tidy < Formula
   url 'ftp://mirror.internode.on.net/pub/gentoo/distfiles/tidy-20090325.tar.bz2'
   sha1 '28c000a2cd40262fc0d7c2c429eb2a09b2df7bf4'
 
+  depends_on :automake
+  depends_on :libtool
+
   def install
     system 'sh', 'build/gnuauto/setup.sh'
     system "./configure", "--disable-debug",
