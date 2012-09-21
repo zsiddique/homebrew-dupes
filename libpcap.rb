0758de622ce9dbe7ad3bf231fc29055b8b69c74c
Will Ross <paxswill@gmail.com>
2012-09-20 21:12:00 -0400

    libpcap: fix checksum
    Closes #93.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- libpcap.rb
+++ libpcap.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libpcap < Formula
   homepage 'http://www.tcpdump.org/'
   url 'http://www.tcpdump.org/release/libpcap-1.3.0.tar.gz'
-  sha1 '67f44a9f1e5ac0c31bce5cefa564c783b27d1e46'
+  sha1 '90aa2f0fcfb4bf79fc50ccdaa70f484d96295be3'
 
   def install
     system "./configure", "--disable-debug",
