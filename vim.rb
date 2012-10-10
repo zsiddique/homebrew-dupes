ee8068ab93962b8c7f3abe64a628aa43d258d29d
Jack Nagel <jacknagel@gmail.com>
2012-10-10 02:05:06 -0500

    vim 7.3.684
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- vim.rb
+++ vim.rb
@@ -4,8 +4,8 @@ class Vim < Formula
   homepage 'http://www.vim.org/'
   # Get stable versions from hg repo instead of downloading an increasing
   # number of separate patches.
-  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-672'
-  version '7.3.672'
+  url 'https://vim.googlecode.com/hg/', :tag => 'v7-3-684'
+  version '7.3.684'
 
   head 'https://vim.googlecode.com/hg/'
 
