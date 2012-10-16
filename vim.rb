9954c11f3e5739c08ec75dc36c7d9f797cce1c5d
Jack Nagel <jacknagel@gmail.com>
2012-10-15 22:04:58 -0500

    vim: add note about Ruby 1.9

--- vim.rb
+++ vim.rb
@@ -37,4 +37,10 @@ class Vim < Formula
     # we still want to install it in the Cellar location.
     system "make", "install", "prefix=#{prefix}"
   end
+
+  def caveats; <<-EOS.undent
+    vim currently does not support Ruby 1.9. Please ensure that the
+    system Ruby (1.8.7) is first in your PATH before installing.
+    EOS
+  end
 end
